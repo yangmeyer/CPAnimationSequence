71013f987b85f7501cdd2c121952101d767e1a84
Yang Meyer <yang.meyer@compeople.de>
2011-07-26 19:19:06 +0200

    Initial Commit

---
+++ AnimationSequence/AnimationSequenceViewController.h
@@ -0,0 +1,15 @@
+//
+//  AnimationSequenceViewController.h
+//  AnimationSequence
+//
+//  Created by Yang Meyer on 26.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimationSequenceViewController : UIViewController {
+    
+}
+
+@end
---
+++ AnimationSequence/AnimationSequenceViewController.m
@@ -0,0 +1,49 @@
+//
+//  AnimationSequenceViewController.m
+//  AnimationSequence
+//
+//  Created by Yang Meyer on 26.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AnimationSequenceViewController.h"
+
+@implementation AnimationSequenceViewController
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return YES;
+}
+
+@end
