133a861d27925c387214ddbab68be0fa02e8d755
Yang Meyer <yang.meyer@compeople.de>
2012-01-02 00:37:13 +0800

    Slow down the demo example.

--- AnimationSequence/AnimationSequenceViewController.m
+++ AnimationSequence/AnimationSequenceViewController.m
@@ -25,32 +25,32 @@
 // execute them at another point later.
 - (CPAnimationStep*) viewSpecificStartAnimation {
 	return [CPAnimationSequence sequenceWithSteps:
-			[CPAnimationStep after:0.5 for:0.3 animate:^{ self.theBox.frame = CGRectMake(150, 150, 100, 100); }],
-			[CPAnimationStep           for:0.3 animate:^{ self.theBox.backgroundColor = [UIColor orangeColor]; }],
-			[CPAnimationStep after:0.5 for:0.5 animate:^{ self.theBox.transform = CGAffineTransformMakeScale(2.0, 2.0); }],
+			[CPAnimationStep after:1.0 for:1.0 animate:^{ self.theBox.frame = CGRectMake(150, 150, 100, 100); }],
+			[CPAnimationStep           for:1.0 animate:^{ self.theBox.backgroundColor = [UIColor orangeColor]; }],
+			[CPAnimationStep after:1.0 for:1.0 animate:^{ self.theBox.transform = CGAffineTransformMakeScale(2.0, 2.0); }],
 			nil
 			];
 }
 
 - (IBAction) startAnimation {
-	[[CPAnimationSequence sequenceWithSteps:
-	  [CPAnimationStep			  for:0.5 animate:^{ self.startButton.alpha = 0.0; }],
+	CPAnimationSequence* animationSequence = [CPAnimationSequence sequenceWithSteps:
+	  [CPAnimationStep for:0.5 animate:^{ self.startButton.alpha = 0.0; }],
 	  [self viewSpecificStartAnimation],
-	  [CPAnimationStep			  for:0.5 animate:^{ self.revertButton.alpha = 1.0; }],
-	  nil
-	] run];
+	  [CPAnimationStep for:0.5 animate:^{ self.revertButton.alpha = 1.0; }],
+	  nil];
+	[animationSequence run];
 }
 
 - (IBAction) revertAnimation {
 	[[CPAnimationSequence sequenceWithSteps:
-	  [CPAnimationStep			 for:0.5 animate:^{ self.revertButton.alpha = 0.0; }],
+	  [CPAnimationStep for:0.5 animate:^{ self.revertButton.alpha = 0.0; }],
 	  [CPAnimationSequence sequenceWithSteps:
-	   [CPAnimationStep after:0.5 for:0.5 animate:^{ self.theBox.transform = CGAffineTransformIdentity; }],
-	   [CPAnimationStep           for:0.3 animate:^{ self.theBox.backgroundColor = [UIColor greenColor]; }],
-	   [CPAnimationStep after:0.5 for:0.3 animate:^{ self.theBox.frame = CGRectMake(100, 100, 100, 100); }],
+	   [CPAnimationStep after:1.0 for:1.0 animate:^{ self.theBox.transform = CGAffineTransformIdentity; }],
+	   [CPAnimationStep           for:1.0 animate:^{ self.theBox.backgroundColor = [UIColor greenColor]; }],
+	   [CPAnimationStep after:1.0 for:1.0 animate:^{ self.theBox.frame = CGRectMake(100, 100, 100, 100); }],
 	   nil
 	   ],
-	  [CPAnimationStep			 for:0.5 animate:^{ self.startButton.alpha = 1.0; }],
+	  [CPAnimationStep for:0.5 animate:^{ self.startButton.alpha = 1.0; }],
 	  nil
 	] run];
 }
