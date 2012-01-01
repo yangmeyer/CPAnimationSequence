cf849dd13b301455d25b177c96ce3f85d5a8d4d5
Yang Meyer <yang.meyer@compeople.de>
2012-01-02 01:17:57 +0800

    Demo now highlights step labels during animation.

--- AnimationSequence/main.m
+++ AnimationSequence/main.m
@@ -1,15 +1,10 @@
-//
-//  main.m
-//  AnimationSequence
-//
+
 //  Created by Yang Meyer on 26.07.11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+//  Copyright 2011-2012 compeopl AG. All rights reserved.
 
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	@autoreleasepool {
 		int retVal = UIApplicationMain(argc, argv, nil, nil);
 		return retVal;
